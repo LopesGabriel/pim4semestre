3e4960880ca17539dc0f0943f3af660be339d4bb
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2019-10-20 14:58:12 -0300

    First Commit

---
+++ src/main/java/com/virtualcondo/VirtualCondoApplication.java
@@ -0,0 +1,13 @@
+package com.virtualcondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCondoApplication.class, args);
+	}
+
+}
